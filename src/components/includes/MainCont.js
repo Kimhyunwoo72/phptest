0129d5ce3748a0693abe46ddfd90ea3ee52e3769
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-04-14 17:13:27 +0900

    0414

--- src/components/includes/MainCont.js
+++ src/components/includes/MainCont.js
@@ -1,13 +1,26 @@
 import React from "react";
 
+function MainInfo({text}) {
+    return <div>{text}</div>
+}
+
+//함수형 컨퍼넌트 * 클래스 컴포넌트  -->리액트 훅 **중요**
+
+const mainText = [
+    {text:"I Provide"},
+    {text:"Visual coding"},
+    {text:"Solutions"},
+    {text:"for you webs"}
+]
+
 function MainCont() {
     return (
+        
     <section className="main__cont">
         <div className="main__inner">
-            <div>We Provide</div>
-            <div>Visual coding</div>
-            <div>Solutions</div>
-            <div>for you webs</div>
+            {mainText.map(txt => (
+                <MainInfo text={txt.text} key={txt.text}/>
+            ))}
         </div>
     </section>
     )
--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import App from "./App";
 
 const rootNode = document.getElementById('root');
 
-ReactDOM.createRoot(rootNode).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(rootNode).render(<App />)   //엄격모두 해제 a링크 바로 안가지는 버그
