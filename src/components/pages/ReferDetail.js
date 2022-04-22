8701ca36e5ed8981651b40f109c2560d8ca4fbde
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-04-22 10:34:08 +0900

    0422

--- src/components/pages/ReferDetail.js
+++ src/components/pages/ReferDetail.js
@@ -3,47 +3,114 @@ import Header from "../layout/Header";
 import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
 import { gsap } from "gsap";
-import Loading from "../basics/Loading";
-import axios from "axios";
 
 
 class ReferDetail extends React.Component {
     componentDidMount(){
-        document.querySelector("body").style.background = "#F0EEEB";
+        const {location, history} = this.props;
+        if(location.state === undefined) {
+            history.push("/reference");
+        }
         this.mainAnimation();
     }
     mainAnimation = () => {
-        gsap.to("#header", {
-            duration: 0.8,
-            top: 0,
-        });
-        gsap.to("#footer", {
-            duration: 0.8,
-            bottom: 0,
-            delay: 0.2,
-        });
-        gsap.to(".refer__inner", {
-            duration: 0.8,
-            bottom: 0,
-            delay: 0.6,
-        });
+        setTimeout(() => {
+            gsap.to("#header", {
+                duration: 0.4,
+                top: 0,
+            });
+            gsap.to("#footer", {
+                duration: 0.4,
+                bottom: 0,
+            })
+        }, 10)
     }
+    // Definition = ({location, definition}) => {
+    //     return (
+    //         <li>
+    //         {location.state.map(der => (
+    //             <Definition der = {location.state.definition}></Definition>
+    //         ))}
+    //         </li>
+    //     )
+    // }
     render(){
-        return (
-            <>
-                <Header color="light" />
-                <Contents>
-                    <section className="refer__cont light">
+        const {location} = this.props;
+        console.log(location.state);
+        if(location.state === undefined){
+            return <div>잘못된 페이지입니다.</div>
+        } else {
+            return (
+                <>
+                    <Header color="light" />
+                    <Contents>
+                    <section className="refer__cont">
                         <div className="container">
                             <div className="refer__inner">
-                                상세페이지 입니다.
+                                <div className="refer__table">
+                                    <h3>{location.state.title}</h3>
+                                    <p>{location.state.desc}</p>
+                                    <table className='table'>
+                                        <thead>
+                                            <tr>
+                                                <th>특징</th>
+                                                <th>설명</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th>요소</th>
+                                                <td>{location.state.element}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>닫는태그</th>
+                                                <td>{location.state.tag}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>사용성</th>
+                                                <td>{location.state.use}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>버전</th>
+                                                <td>{location.state.version}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>시각적표현</th>
+                                                <td>{location.state.view}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <ul>
+                                        <li>
+                                            <h4>정의 (Definition)</h4>
+                                            <ul>
+                                                function Definition() {
+                                                    
+                                                }
+                                                <p><li>{location.state.Definition}</li></p>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <h4>접근성 (Accessibility)</h4>
+                                            <p>{location.state.Accessibility}</p>
+                                        </li>
+                                        <li>
+                                            <h4>참고사이트 (Reference)</h4>
+                                            <ul>
+                                                <li><a href={location.state.mdn}>MDN</a></li>
+                                                <li><a href={location.state.w3c}>W3C</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </section>
-                </Contents>
-                <Footer color="light" />
-            </>
-        )
+                    </Contents>
+                    <Footer color="light" />
+                </>
+            )
+        }
     }
 }
 export default ReferDetail
--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -5,26 +5,14 @@ import ContTitle from "../layout/ContTitle";
 import ContContact from "../layout/ContContact";
 import ReferenceCont from "../includes/ReferenceCont";
 import Footer from "../layout/Footer";
-import { gsap } from "gsap";
 import Loading from "../basics/Loading";
+import { gsap } from "gsap";
 import axios from "axios";
+import ReferCont from "../includes/ReferenceCont";
 
-// function Reference() {
-//     return (
-//         <>
-//             <Header color = "light" />
-//             <Contents>
-//                 <ContTitle title = {["HTML", "REFERENCE", "light" ]}/>
-//                     <ReferenceCont color = "light" />
-//                 <ContContact />
-//                 <Footer color = "light" />
-//             </Contents>
-//         </>
-//     )
-    
-// }
 
 class Reference extends React.Component {
+    //state는 (함수 내에 선언된 변수처럼) 컴포넌트 안에서 관리됩니다.
     state = {
         isLoading: true,
         refers: []
@@ -70,7 +58,7 @@ class Reference extends React.Component {
 
 
     //async & axios는 다 다운받고 실행해라라는 명령어
-    getSite = async ()=>{
+    getRefers = async ()=>{
         //{ data : { data: {htmlRefer}}} 은 경로의 파일만 가져오게 설정하는 것.
         const {
              data : {
@@ -78,16 +66,14 @@ class Reference extends React.Component {
                 },
             } = await axios.get("https://kimhyunwoo72.github.io/react2022/src/assets/json/referencs.json") 
                 this.setState({refers: htmlRefer, isLoading: false})
-                console.log("두번째 시작")
                 this.mainAnimation()
     }
 
-    componentDidMount(){
+    componentDidMount() {
         setTimeout(() => {
-            console.log("첫번째 시작")
             document.getElementById("loading").classList.remove("loading__active")
-            document.querySelector("body").style.background = "#f0eeeb"
-            this.getSite()
+            document.querySelector("body").style.background = "#000"
+            this.getRefers()
         }, 2000);
     }
 
@@ -98,15 +84,44 @@ class Reference extends React.Component {
         return (
             <>
                 {isLoading ? (
-                    <Loading color = "light" />
+                    <Loading/>
                 ) : (
                     <>
-                        <Header color = "light" />
+                        <Header/>
                         <Contents>
-                            <ContTitle title = {["HTML", "REFERENCE", "light" ]}/>
-                                <ReferenceCont refers = {refers} color = "light" />
+                            <ContTitle title = {["HTML", "REFERENCE"]}/>
+                                {/* <ReferenceCont refers = {refers} color = "light" /> */}
+                                <section className="refer__cont">
+                                    <div className="container">
+                                        <div className="refer__inner">
+                                            <h2>CSS</h2>
+                                            <ul className="refer__list">
+                                                {refers.map((refer)=>(
+                                                    <ReferCont 
+                                                    key={refer.id}
+                                                    id={refer.id}
+                                                    title={refer.title}
+                                                    desc={refer.desc}
+                                                    use={refer.use}
+                                                    desc2 ={refer.desc2}
+                                                    element ={refer.element}
+                                                    tag ={refer.tag}
+                                                    view ={refer.view}
+                                                    image ={refer.image}
+                                                    link ={refer.link}
+                                                    Definition ={refer.Definition}
+                                                    Accessibility ={refer.Accessibility}
+                                                    w3c ={refer.w3c}
+                                                    mdn ={refer.mdn}
+                                                    version ={refer.version}
+                                                    />
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </section>
                             <ContContact />
-                            <Footer color = "light" />
+                            <Footer/>
                         </Contents>
                     </>
                 )}
